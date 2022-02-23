52b82a70407aba3e6b02d4984b6d4e1bb53f317f
sangzun-han <hsjun1996@naver.com>
2022-02-24 00:32:20 +0900

    장바구니 데이터수정

--- src/components/cart/cartItem.jsx
+++ src/components/cart/cartItem.jsx
@@ -3,54 +3,79 @@ import { getProductDetail } from "../../service/fetcher";
 import styles from "./cartItem.module.css";
 
 const CartItem = ({ product }) => {
-  console.log(product);
+  const { product_id, quantity } = product;
   const [info, setInfo] = useState({});
+  const [count, setCount] = useState(quantity);
+
+  const convertPrice = () => {
+    return info.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
+
+  const totalConvertPrice = () => {
+    return (info.price * count)
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
+
+  const onIncrease = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const onDecrease = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
 
   useEffect(() => {
-    getProductDetail(product.product_id).then((res) => {
-      console.log(res.data);
+    getProductDetail(product_id).then((res) => {
       setInfo(res.data);
     });
-  }, [product.product_id]);
+  }, [product_id]);
+
   return (
-    <section className={styles.cart_product_list}>
-      <input type="checkbox" />
-      <div className={styles.cart_product_wrap}>
-        <div className={styles.cart_product_image}>
-          <img src={info.image} alt="product-img" />
-        </div>
+    <>
+      {info.image && (
+        <section className={styles.cart_product_list}>
+          <input type="checkbox" />
+          <div className={styles.cart_product_wrap}>
+            <div className={styles.cart_product_image}>
+              <img src={info.image} alt="product-img" />
+            </div>
 
-        <div className={styles.cart_product_info}>
-          <p className={styles.seller_store}>{info.seller_store}</p>
-          <p className={styles.product_name}>{info.product_name}</p>
-          <p className={styles.price}>{info.price}</p>
-          <p className={styles.delivery}>택배배송 / 무료배송</p>
-        </div>
-      </div>
+            <div className={styles.cart_product_info}>
+              <p className={styles.seller_store}>{info.seller_store}</p>
+              <p className={styles.product_name}>{info.product_name}</p>
+              <p className={styles.price}>{convertPrice()}</p>
+              <p className={styles.delivery}>택배배송 / 무료배송</p>
+            </div>
+          </div>
 
-      <div className={styles.cart_product_count}>
-        <img
-          className={styles.minus}
-          src="/images/icon-minus-line.svg"
-          alt="minus"
-        />
+          <div className={styles.cart_product_count}>
+            <img
+              className={styles.minus}
+              src="/images/icon-minus-line.svg"
+              alt="minus"
+              onClick={onDecrease}
+            />
 
-        <div className={styles.count}>
-          <span>{product.quantity}</span>
-        </div>
+            <div className={styles.count}>
+              <span>{count}</span>
+            </div>
 
-        <img
-          className={styles.plus}
-          src="/images/icon-plus-line.svg"
-          alt="plus"
-        />
-      </div>
+            <img
+              className={styles.plus}
+              src="/images/icon-plus-line.svg"
+              alt="plus"
+              onClick={onIncrease}
+            />
+          </div>
 
-      <div className={styles.cart_product_price}>
-        <p className={styles.total_price}>29000원</p>
-        <button className={styles.btn_submit}>주문하기</button>
-      </div>
-    </section>
+          <div className={styles.cart_product_price}>
+            <p className={styles.total_price}>{totalConvertPrice()}</p>
+            <button className={styles.btn_submit}>주문하기</button>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 
