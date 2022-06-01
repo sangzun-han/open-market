4f2f7f5233fc0b63d425720d3208bf4c11ed55af
sangzun-han <hsjun1996@naver.com>
2022-06-02 00:15:49 +0900

    get product detail

--- src/components/product_detail/detail.jsx
+++ src/components/product_detail/detail.jsx
@@ -1,12 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { TopNavigationBar } from "../header/topNavigationBar/topNavigationBar";
 
 export const Detail = () => {
   const { id } = useParams();
+  const [product, setProduct] = useState({});
+  useEffect(() => {
+    const apiUrl = "/data/products.json";
 
+    axios.get(apiUrl).then((data) => {
+      setProduct(
+        data.data.products.find((product) => product.id === parseInt(id))
+      );
+    });
+  }, []);
   return (
     <>
       <TopNavigationBar />
+      {product.id}
     </>
   );
 };
