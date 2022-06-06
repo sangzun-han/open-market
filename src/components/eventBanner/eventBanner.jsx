deca9ccb3c9a030c1111bfb94df9c450c2b663bd
sangzun-han <hsjun1996@naver.com>
2022-06-06 18:30:59 +0900

    eventBanner

--- src/components/eventBanner/eventBanner.jsx
+++ src/components/eventBanner/eventBanner.jsx
@@ -2,7 +2,13 @@ import styles from "./eventBanner.module.css";
 
 export const EventBanner = () => {
   return (
-    <article className={styles.banner}>
+    <article
+      className={styles.banner}
+      style={{
+        backgroundImage: "url(/images/event.png",
+        backgroundSize: "100%",
+      }}
+    >
       <div className={styles.right}>
         <img src="images/icon-swiper-2.svg" alt="right" />
       </div>
