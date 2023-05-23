221366f3fdd7dcc64b0d673f6b36e69e6a24ee89
한상준 <hsjun1996@naver.com>
2023-05-23 10:25:23 +0900

    Delete : project

--- src/pages/basket.jsx
+++
@@ -1,7 +0,0 @@
-import { Cart } from "../components/cart/cart";
-
-const Basket = ({ cart, setCart, convertPrice }) => {
-  return <Cart cart={cart} setCart={setCart} convertPrice={convertPrice} />;
-};
-
-export default Basket;
